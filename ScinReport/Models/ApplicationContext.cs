12a469b473e05e586868ae63f442fbe9afcf7354
Олег Бутрий <oleha01@butryy.onmicrosoft.com>
2019-05-20 13:57:38 +0300

    SaveAll

---
+++ ScinReport/Models/ApplicationContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ScinReport.Models
+{
+    public class ApplicationContext : IdentityDbContext<User>
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
