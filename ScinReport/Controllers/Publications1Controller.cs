f33f43e818210bf485890df2021971ab06e0583b
romankoltun <rmkoltun3@gmail.com>
2019-05-21 14:32:17 +0300

    controller  publication

---
+++ ScinReport/Controllers/Publications1Controller.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ScinReport.Models;
+
+namespace ScinReport.Controllers
+{
+    public class Publications1Controller : Controller
+    {
+        private readonly ApplicationContext _context;
+
+        public Publications1Controller(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Publications1
+        public async Task<IActionResult> Index()
+        {
+            var applicationContext = _context.Publications.Include(p => p.Type);
+            return View(await applicationContext.ToListAsync());
+        }
+
+        // GET: Publications1/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var publication = await _context.Publications
+                .Include(p => p.Type)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (publication == null)
+            {
+                return NotFound();
+            }
+
+            return View(publication);
+        }
+
+        // GET: Publications1/Create
+        public IActionResult Create()
+        {
+            ViewData["TypeId"] = new SelectList(_context.Work_Enums, "Id", "Id");
+            return View();
+        }
+
+        // POST: Publications1/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,TypeId,Date,Status")] Publication publication)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(publication);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["TypeId"] = new SelectList(_context.Work_Enums, "Id", "Id", publication.TypeId);
+            return View(publication);
+        }
+
+        // GET: Publications1/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var publication = await _context.Publications.FindAsync(id);
+            if (publication == null)
+            {
+                return NotFound();
+            }
+            ViewData["TypeId"] = new SelectList(_context.Work_Enums, "Id", "Id", publication.TypeId);
+            return View(publication);
+        }
+
+        // POST: Publications1/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,TypeId,Date,Status")] Publication publication)
+        {
+            if (id != publication.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(publication);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PublicationExists(publication.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["TypeId"] = new SelectList(_context.Work_Enums, "Id", "Id", publication.TypeId);
+            return View(publication);
+        }
+
+        // GET: Publications1/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var publication = await _context.Publications
+                .Include(p => p.Type)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (publication == null)
+            {
+                return NotFound();
+            }
+
+            return View(publication);
+        }
+
+        // POST: Publications1/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var publication = await _context.Publications.FindAsync(id);
+            _context.Publications.Remove(publication);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PublicationExists(int id)
+        {
+            return _context.Publications.Any(e => e.Id == id);
+        }
+    }
+}
