3b45c3c915b4f4d3fa205b7143f111578e3a5d1f
Ira Burdeina <iruna.burdeina@gmail.com>
2019-05-29 00:02:25 +0300

    Add Title Tests

---
+++ ScinReport_Tests/TitleTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using ScinReport.Models;
+namespace ScinReport_Tests
+{
+    public class TitleTest
+    {
+        
+        [Fact]
+        public void CanChangeTitleText()
+        {
+            var u = new Title {  Id=1,Text="Cursova",CafedraId=1,Years="2019"};
+            u.Text = "Cursova written by Iryna";
+            Assert.Equal("Cursova written by Iryna", u.Text);
+        }
+        [Fact]
+        public void CanChangeTitleId()
+        {
+            var u = new Title { Id = 1, Text = "Cursova", CafedraId = 1, Years = "2019" };
+            u.Id = 3;
+            Assert.Equal(3, u.Id);
+        }
+        [Fact]
+        public void CanChangeTitleCafedraId()
+        {
+            var u = new Title { Id = 1, Text = "Cursova", CafedraId = 1, Years = "2019" };
+            u.CafedraId = 3;
+            Assert.Equal(3, u.CafedraId);
+        }
+        [Fact]
+        public void CanChangeTitleYears()
+        {
+            var u = new Title { Id = 1, Text = "Cursova", CafedraId = 1, Years = "2019" };
+            u.Years = "2018-2019";
+            Assert.Equal("2018-2019", u.Years);
+        }
+    }
+}
