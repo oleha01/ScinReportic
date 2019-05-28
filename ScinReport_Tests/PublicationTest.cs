219f299d287c0b7bb27099718323b0b7dc96add8
Ira Burdeina <iruna.burdeina@gmail.com>
2019-05-28 23:39:13 +0300

    Add Publication test

---
+++ ScinReport_Tests/PublicationTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using ScinReport.Models;
+namespace ScinReport_Tests
+{
+    public class PublicationTest
+    {
+        public int Id { get; set; }
+        public int TypeId { get; set; }
+        virtual public WorkEnum Type { get; set; }
+        public DateTime Date { get; set; }
+        public string Status { get; set; }
+        [Fact]
+        public void CanChangePublicationTypeId()
+        {
+            var u = new Publication { Id = 1, TypeId = 12,Status="In Progress" };
+            u.TypeId = 2;
+            Assert.Equal(2, u.TypeId);
+        }
+        [Fact]
+        public void CanChangePublicationId()
+        {
+            var u = new Publication { Id = 1, TypeId = 12, Status = "In Progress" };
+            u.Id = 34;
+            Assert.Equal(34, u.Id);
+        }
+        [Fact]
+        public void CanChangePublicationStatus()
+        {
+            var u = new Publication { Id = 1, TypeId = 12, Status = "In Progress" };
+            u.Status = "Comleted";
+            Assert.Equal("Comleted", u.Status);
+        }
+    }
+}
