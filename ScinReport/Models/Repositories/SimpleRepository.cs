08d96f7c14c9a473cf850f34f4f0e81c0ba97dbf
Ira Burdeina <iruna.burdeina@gmail.com>
2019-05-29 02:03:41 +0300

    Add PublicationController Test

---
+++ ScinReport/Models/Repositories/SimpleRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ScinReport.Models.Repositories
+{
+    public class SimpleRepository:IRepository
+    {
+        private static SimpleRepository sharedRepository = new SimpleRepository();
+        private Dictionary<int, Publication> publications
+        = new Dictionary<int, Publication>();
+        public static SimpleRepository SharedRepository => sharedRepository;
+        public SimpleRepository()
+        {
+            var initialItems = new[] {
+                 new Publication  { Id = 1, TypeId = 12,Status="In Progress" },
+                 new Publication { Id = 2, TypeId = 1,Status="In Progress" },
+                 new Publication { Id = 3, TypeId = 13,Status="In Progress" },
+                 new Publication { Id = 4, TypeId = 2,Status="In Progress" }
+                 };
+            foreach (var p in initialItems)
+            {
+                AddPublication(p);
+            }
+            publications.Add(-1, null);
+        }
+        public IEnumerable<Publication> Publications => publications.Values;
+        public void AddPublication (Publication p) => publications.Add(p.Id, p);
+    }
+}
---
+++ ScinReport_Tests/ControllerTests/PublicationControllerTest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ScinReport.Models;
+using ScinReport.Controllers;
+using Xunit;
+namespace ScinReport_Tests.ControllerTests
+{
+    public class PublicationControllerTest
+    {
+    }
+}
