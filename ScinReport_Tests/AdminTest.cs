b341e9f94ead77c0eef09485cc3d1ae60e602df6
Ira Burdeina <iruna.burdeina@gmail.com>
2019-05-28 22:54:59 +0300

    Add UnitTest for Admin

---
+++ ScinReport_Tests/AdminTest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using ScinReport.Models;
+
+namespace ScinReport_Tests
+{
+    public class AdminTest
+    {
+        
+        [Fact]
+        public void CanChangeAdminName()
+        {
+            var u = new Admin {Id=1, SurName = "Burdeina", Name = "Ira", Patronymic = "Mikitivna",UserId=12 };
+            u.Name = "Iryna";
+            Assert.Equal("Iryna", u.Name);
+        }
+        [Fact]
+        public void CanChangeAdminId()
+        {
+            var u = new Admin { Id = 1, SurName = "Burdeina", Name = "Ira", Patronymic = "Mikitivna", UserId = 12 };
+            u.Id = 3;
+            Assert.Equal(3, u.Id);
+        }
+        [Fact]
+        public void CanChangeAdminSurName()
+        {
+            var u = new Admin { Id = 1, SurName = "Burdeina", Name = "Ira", Patronymic = "Mikitivna", UserId = 12 };
+            u.SurName = "Burdeina";
+            Assert.Equal("Burdeina", u.SurName);
+        }
+        [Fact]
+        public void CanChangeAdminPatronymic()
+        {
+            var u = new Admin { Id = 1, SurName = "Burdeina", Name = "Ira", Patronymic = "Mikitivna", UserId = 12 };
+            u.Patronymic = "Mykytivna";
+            Assert.Equal("Mykytivna", u.Patronymic);
+        }
+        [Fact]
+        public void CanChangeAdminUserId()
+        {
+            var u = new Admin { Id = 1, SurName = "Burdeina", Name = "Ira", Patronymic = "Mikitivna", UserId = 12 };
+            u.UserId = 34;
+            Assert.Equal(34, u.UserId);
+        }
+    }
+}
--- ScinReport_Tests/UserTest.cs
+++ ScinReport_Tests/UserTest.cs
@@ -7,16 +7,7 @@ namespace ScinReport_Tests
 {
     public class UserTest
     {
-        //public string SurName { get; set; }
-        //public string Name { get; set; }
-        //public string Patronymic { get; set; } //По-Батькові
-        //public string Position { get; set; }
-        //public int Year_of_birth { get; set; }
-        //public int Year_of_graduation { get; set; }//закінчення внз
-        //public int Year_of_Protection { get; set; }//рік захисту
-        //public string Academic_status { get; set; }//вчене звання
-        //public int Year_of_Assignment { get; set; }//рік присвоєння вченого звання
-        //public string Degree { get; set; }//ступінь
+        
         [Fact]
         public void CanChangeUserName()
         {
