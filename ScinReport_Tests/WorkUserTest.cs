9353aca725008c76ec8951943fd191ae6e4788a1
Ira Burdeina <iruna.burdeina@gmail.com>
2019-06-01 22:17:49 +0300

    Fix WorkUserTest

--- ScinReport_Tests/WorkUserTest.cs
+++ ScinReport_Tests/WorkUserTest.cs
@@ -10,23 +10,23 @@ namespace ScinReport_Tests
         [Fact]
         public void CanChangeWorkUserId()
         {
-            var u = new Work_User { Id = 3, WorkId = 4, UserId = 5 };
+            var u = new Work_User { Id = 3, PublicationId = 4, UserId = "5" };
             u.Id = 4;
             Assert.Equal(4, u.Id);
         }
         [Fact]
-        public void CanChangeWorkUserWorkId()
+        public void CanChangeWorkUserPublicationId()
         {
-            var u = new Work_User { Id = 3, WorkId = 4, UserId = 5 };
-            u.WorkId = 4;
-            Assert.Equal(4, u.WorkId);
+            var u = new Work_User { Id = 3, PublicationId = 2, UserId = "5" };
+            u.PublicationId = 4;
+            Assert.Equal(4, u.PublicationId);
         }
         [Fact]
         public void CanChangeWorkUserUserId()
         {
-            var u = new Work_User { Id = 3, WorkId = 4, UserId = 5 };
-            u.UserId = 4;
-            Assert.Equal(4, u.UserId);
+            var u = new Work_User { Id = 3, PublicationId = 4, UserId = "5" };
+            u.UserId = "6";
+            Assert.Equal("6", u.UserId);
         }
     }
 }
